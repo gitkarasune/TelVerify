687b24b0c019196fd7937bb4028b46cf1d9c6f60
unknown <sunebarikara@gmail.com>
2025-01-29 19:16:27 +0100

    new commit

--- src/Components/Docs.tsx
+++ src/Components/Docs.tsx
@@ -19,7 +19,7 @@ const Docs = () => {
       </section>
 
       <div className="grid grid-col-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 mb-7 gap-4">
-        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-black text-white text-sm hover:border-blue-900 gradient-bg">
+        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-[#101010] text-white text-sm hover:border-blue-900 gradient-bg">
           <h2 className="text-2xl font-bold mb-5 Telverify">Overview</h2>
           <p>
             <mark>TelVerify</mark> is a simple and efficient tool for verifying
@@ -30,7 +30,7 @@ const Docs = () => {
             for phone numbers.
           </p>
         </section>
-        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-black text-white text-sm hover:border-blue-900 gradient-bg">
+        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-[#101010] text-white text-sm hover:border-blue-900 gradient-bg">
           <h2 className="text-2xl font-bold Telverify mb-5">Features</h2>
           <ul className="list-disc list-inside">
             <li>Validate Nigerian phone numbers.</li>
@@ -42,7 +42,7 @@ const Docs = () => {
             <li>Lightweight and scalable solution.</li>
           </ul>
         </section>
-        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-black text-white text-sm hover:border-blue-900 gradient-bg">
+        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-[#101010] text-white text-sm hover:border-blue-900 gradient-bg">
           <h2 className="text-2xl font-bold Telverify mb-5">Use Cases</h2>
           <ul className="list-disc list-inside">
             <li>Optimize SMS gateway routing.</li>
@@ -50,8 +50,10 @@ const Docs = () => {
             <li>Enhance fraud prevention systems with carrier verification.</li>
           </ul>
         </section>
-        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-black text-white text-sm hover:border-blue-900 gradient-bg">
-          <h2 className="text-2xl font-bold Telverify mb-5">Acknowledgements</h2>
+        <section className="border border-gray-700 p-4 rounded-2xl transition-all bg-[#101010] text-white text-sm hover:border-blue-900 gradient-bg">
+          <h2 className="text-2xl font-bold Telverify mb-5">
+            Acknowledgements
+          </h2>
           <ul className="list-disc list-inside">
             <li>React</li>
             <li>TypeScript</li>
@@ -73,7 +75,9 @@ const Docs = () => {
           <ol className="list-decimal list-inside">
             <li>
               Open your browser and navigate to{" "}
-              <mark><code>http://tel-verify.vercel.app</code></mark>
+              <mark>
+                <code>http://tel-verify.vercel.app</code>
+              </mark>
             </li>
             <li>
               Enter a Nigerian phone number in the input field. The input field
@@ -96,6 +100,54 @@ const Docs = () => {
           </ol>
         </section>
       </div>
+
+      <h1 className="text-3xl mb-2 font-bold Telverify" style={{ paddingTop: "32px" }}>
+        Subscribe
+      </h1>
+      <footer
+        className="bg-black text-white p-8 rounded"
+        style={{ paddingBottom: "32px" }}
+      >
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="mb-4 md:mb-0">
+              <h2 className="text-2xl font-bold Telverify">
+                Subscribe to our Newsletter
+              </h2>
+              <p className="text-gray-400 text-center md:text-left lg:text-left xl:text-left 2xl:text-left">
+                Get the latest updates and offers.
+              </p>
+            </div>
+            <form className="flex flex-col md:flex-row items-center">
+              <input
+                type="email"
+                placeholder="Example@gmail.com"
+                className="p-2 rounded-sm bg-gray-900 text-black mb-2 md:mb-0 md:mr-2"
+                required
+              />
+              <button
+                type="submit"
+                className="bg-white text-black font-medium p-2 rounded-sm text-sm transition-all"
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+          <div className="mt-8 text-center md:text-left">
+            <p className="text-sm">
+              &copy; {new Date().getFullYear()} TelVerify. All rights reserved.
+            </p>
+            <div className="flex justify-center md:justify-start mt-4">
+              <Link to="/" className="text-gray-400 mx-2 Telverify">
+                Privacy Policy
+              </Link>
+              <Link to="/" className="text-gray-400 mx-2 Telverify">
+                Terms of Service
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
